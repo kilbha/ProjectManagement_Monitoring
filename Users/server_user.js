13f938018cea9cb113cc0d7fe3e9920c55f3cc79
Bhargav-Synfosys <110816674+Bhargav-Synfosys@users.noreply.github.com>
2023-09-02 15:31:59 +0530

    Created promise for starting server

--- Users/server_user.js
+++ Users/server_user.js
@@ -1,13 +1,31 @@
-'use strict'
+"use strict";
 
 const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(morgan('combined'));
-app.use(helmet())
+app.use(morgan("combined"));
+app.use(helmet());
 
-app.listen(() => console.log(`Users microservice running on PORT ${PORT}`))
+const startServer = (port) => {
+  return new Promise((resolve, reject) => {
+    const server = app.listen(port, () => {
+      resolve(server);
+    });
+    server.on("error", (error) => {
+      reject(error);
+    });
+  });
+};
+
+// app.listen(() => console.log(`Users microservice running on PORT ${PORT}`))
+startServer(PORT)
+  .then((server) => {
+    console.log(`Users microservice running on PORT ${PORT}`);
+  })
+  .catch((error) => {
+    console.error("Error starting server:", error);
+  });
